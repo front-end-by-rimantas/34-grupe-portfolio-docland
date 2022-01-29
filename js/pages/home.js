cc8b05f4822297c334985bdbe0a42e2635a37bb4
Rimantas <belauzas@yahoo.com>
2022-01-29 12:45:59 +0200

    Git conflict preventing and file structure #2

---
+++ js/pages/home.js
@@ -0,0 +1,39 @@
+// import
+import { Header } from "../components/header/Header.js";
+import { headerMenuData } from "../data/headerMenuData.js";
+
+// execution
+
+/* header start */
+new Header(headerMenuData);
+/* header end */
+
+/* hero start */
+/* hero end */
+
+/* calendar start */
+/* calendar end */
+
+/* course carousel start */
+/* course carousel end */
+
+/* course syllabus start */
+/* course syllabus end */
+
+/* course benefit start */
+/* course benefit end */
+
+/* pricing start */
+/* pricing end */
+
+/* student reviews start */
+/* student reviews end */
+
+/* newsletter start */
+/* newsletter end */
+
+/* course instructor start */
+/* course instructor end */
+
+/* footer start */
+/* footer end */
